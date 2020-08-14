e562d4e30f5e5d8d1de9f99460a3c61e96fa4c8f
Eron Cohen <eroncohen10@gmail.com>
2020-08-14 12:37:14 +0300

    add doors

---
+++ Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    public Animator animator;
+    public bool isOpen;
+    //public GameObject door_axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("OpenTrigger");
+            isOpen = true;
+        }
+    }
+   
+    void OnTriggerExit(Collider other)
+    {
+        if (isOpen)
+        {
+            animator.SetTrigger("CloseTrigger");
+            isOpen = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GlassDoorMotion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassDoorMotion : MonoBehaviour
+{
+    public bool isOpen;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="MainCamera")
+        {
+            isOpen = true;
+            animator.SetTrigger("Open");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            animator.SetTrigger("Close");
+        }
+    }
+}
